4595ec4092df4a97b36971d5da87fd6e902610b6
Matheus Cardoso <dacachmatheus@gmail.com>
2022-06-16 18:52:03 -0300

    Use Vec<char> and filter
    Thanks to @bobahop for mentoring!

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,30 +1,30 @@
 use std::collections::HashSet;
 
-fn sort_word_case_insensitive(word: &str) -> String {
-    let word = word.to_lowercase();
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let word_lowercase = word.to_lowercase();
+    let word_sorted = get_sorted(&word_lowercase);
+
+    possible_anagrams
+        .iter()
+        .filter(|candidate| {
+            let candidate = candidate.to_lowercase();
+            // Length between cases may differ in some cases, so we compare
+            // them both lowercase
+            if candidate.len() != word_lowercase.len() {
+                return false;
+            }
+            if candidate == word_lowercase {
+                return false;
+            }
+            get_sorted(&candidate) == word_sorted
+        })
+        .copied()
+        .collect()
+}
+
+fn get_sorted(word: &str) -> Vec<char> {
     let mut as_chars: Vec<char> = word.chars().collect();
     as_chars.sort_unstable();
 
-    as_chars.iter().collect()
-}
-
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let mut sorted_word_list = Vec::new();
-    for w in possible_anagrams {
-        sorted_word_list.push(sort_word_case_insensitive(w));
-    }
-
-    let target_word = sort_word_case_insensitive(word);
-
-    let lowercase_input_word = word.to_lowercase();
-    let mut results = HashSet::new();
-    for (sorted_word, original_word) in sorted_word_list.iter().zip(possible_anagrams) {
-        let is_anagram = *sorted_word == target_word;
-        let same_as_input = original_word.to_lowercase() == lowercase_input_word;
-        if is_anagram && !same_as_input {
-            results.insert(*original_word);
-        }
-    }
-
-    results
+    as_chars
 }
