25366e408a03c84f4283a27f9476fe54d0046724
Matheus Cardoso <dacachmatheus@gmail.com>
2022-06-20 22:52:01 -0300

    Use usize for coordinates
    We can make use of saturating_sub and min functions to stay within the field's bounds.

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -12,7 +12,7 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
     for (x, row) in field.iter().enumerate() {
         let mut current_string = String::new();
         for (y, col) in row.iter().enumerate() {
-            match count_mines(&field, x as i32, y as i32) {
+            match count_mines(&field, x, y) {
                 None => current_string.push('*'),
                 Some(0) => current_string.push(' '),
                 Some(count) => current_string.push_str(count.to_string().as_str()),
@@ -24,14 +24,19 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
     result
 }
 
-fn count_mines(field: &[&[u8]], x: i32, y: i32) -> Option<usize> {
+fn count_mines(field: &[&[u8]], x: usize, y: usize) -> Option<usize> {
     if is_mine(field, x, y) {
         return None;
     }
 
+    let height = field.len();
+    let width = field[0].len();
+
     let mut surrounding_mines = 0;
-    for neighbor_x in x - 1..=x + 1 {
-        for neighbor_y in y - 1..=y + 1 {
+    let (x_lower, x_upper) = (x.saturating_sub(1), (x + 1).min(height - 1));
+    let (y_lower, y_upper) = (y.saturating_sub(1), (y + 1).min(width - 1));
+    for neighbor_x in x_lower..=x_upper {
+        for neighbor_y in y_lower..=y_upper {
             if is_mine(field, neighbor_x, neighbor_y) {
                 surrounding_mines += 1;
             }
@@ -41,13 +46,6 @@ fn count_mines(field: &[&[u8]], x: i32, y: i32) -> Option<usize> {
     Some(surrounding_mines)
 }
 
-fn is_in_bounds(field: &[&[u8]], x: i32, y: i32) -> bool {
-    let height = field.len() as i32;
-    let width = field[0].len() as i32;
-
-    x >= 0 && x < height && y >= 0 && y < width
-}
-
-fn is_mine(field: &[&[u8]], x: i32, y: i32) -> bool {
-    is_in_bounds(field, x, y) && field[x as usize][y as usize] == b'*'
+fn is_mine(field: &[&[u8]], x: usize, y: usize) -> bool {
+    field[x][y] == b'*'
 }
