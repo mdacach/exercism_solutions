bc16aee69f1d3fcedea12c0b7a8f38123fd205c0
Matheus Cardoso <dacachmatheus@gmail.com>
2022-06-26 13:34:29 -0300

    Refactor into nested match for code reuse

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -20,21 +20,15 @@ pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
     for symbol in inputs {
         match symbol {
             CalculatorInput::Value(v) => stack.push(*v),
-            CalculatorInput::Add => {
+            _ => {
                 let (rhs, lhs) = pop_two(&mut stack)?;
-                stack.push(lhs + rhs);
-            }
-            CalculatorInput::Subtract => {
-                let (rhs, lhs) = pop_two(&mut stack)?;
-                stack.push(lhs - rhs);
-            }
-            CalculatorInput::Multiply => {
-                let (rhs, lhs) = pop_two(&mut stack)?;
-                stack.push(lhs * rhs);
-            }
-            CalculatorInput::Divide => {
-                let (rhs, lhs) = pop_two(&mut stack)?;
-                stack.push(lhs / rhs);
+                match symbol {
+                    CalculatorInput::Add => stack.push(lhs + rhs),
+                    CalculatorInput::Subtract => stack.push(lhs - rhs),
+                    CalculatorInput::Multiply => stack.push(lhs * rhs),
+                    CalculatorInput::Divide => stack.push(lhs / rhs),
+                    _ => (),
+                }
             }
         }
     }
